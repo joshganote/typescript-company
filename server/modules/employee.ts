746db930d9d2f49d08c5d8c4702262b1c5c978ee
Josh Ganote <j.ganote0825@gmail.com>
2020-03-03 12:33:27 -0600

    imported the position enum and created an employee class

--- server/modules/employee.ts
+++ server/modules/employee.ts
@@ -1,12 +1,17 @@
+import { Position } from "../enums/Position";
+
+
 export class Employee {
 
     private firstName: string;
     private lastName: string;
     private salary: number;
+    private position: Position;
 
-    constructor(firstName: string, lastName: string, salary: number) {
+    constructor(firstName: string, lastName: string, salary: number, position: Position) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.salary = salary; 
+        this.salary = salary;
+        this.position = position;
     }
 }
